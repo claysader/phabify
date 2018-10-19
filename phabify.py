a97b83fbb26a0e0ccfa77206dd868a5c7287c85e
Clayton Sader <claytonsader@Claytons-MacBook-Pro.local>
2018-10-19 15:03:57 +0300

    v5

--- phabify.py
+++ phabify.py
@@ -4,13 +4,17 @@ from flask import Flask, request #import main Flask class and request object
 
 app = Flask(__name__) #create the Flask app
 
-@app.route('/phabify', methods=['POST']) #GET requests will be blocked
-def phabify():
-    return 'Todo...'
-
 @app.route('/query-example')
 def query_example():
     return 'Todo...'
 
+@app.route('/form-example')
+def form_example():
+    return 'Todo...'
+
+@app.route('/json-example')
+def json_example():
+    return 'Todo...'
+
 if __name__ == '__main__':
-    app.run() #run app in debug mode on port 5000
+    app.run(debug=False) #run app in debug mode on port 5000
