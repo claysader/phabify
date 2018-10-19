099b54df898a139f5cb8b3d79f414608b9215507
Clayton Sader <claytonsader@Claytons-MacBook-Pro.local>
2018-10-19 18:32:45 +0300

    v5

--- phabify.py
+++ phabify.py
@@ -6,29 +6,8 @@ import json
 
 app = Flask(__name__) #create the Flask app
 
-@app.route('/query-example')
-def query_example():
-
-	text = "I fixed T4523 and t8245"
-
-	pattern = "[Tt][0-9]{4}"
-	matches = re.findall(pattern, text)
-
-	linkText =  "Phab tickets detected: "
-
-	for match in matches:
-		phabLink = "https://phab.zenysis.com/" + match + " "
-		linkText = linkText + phabLink
-
-	return linkText
-
-@app.route('/test', methods=['POST'])
-def form_example():
-    return request
-
 @app.route('/', methods=['POST'])
-def json_parse():
-#    req_data = request.get_json()
+def post_parse():
 
     text = request.form.get('text')
     user = request.form.get('user_name')
