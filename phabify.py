56e54429750718f41881a718838e2649f7878298
Clayton Sader <claytonsader@Claytons-MacBook-Pro.local>
2018-10-19 15:19:42 +0300

    v5

--- phabify.py
+++ phabify.py
@@ -17,4 +17,4 @@ def json_example():
     return 'Todo...'
 
 if __name__ == '__main__':
-    app.run(debug=False, port=environ.get("PORT", 5000), host='0.0.0.0') #run app in debug mode on port 5000
+    app.run(debug=False, host='0.0.0.0') #run app in debug mode on port 5000
