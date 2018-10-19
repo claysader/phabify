791d9744b36e35aa47ff09f3e270cb9c495d3609
Clayton Sader <claytonsader@Claytons-MacBook-Pro.local>
2018-10-19 15:50:44 +0300

    v5

--- phabify.py
+++ phabify.py
@@ -1,12 +1,25 @@
 #phabify.py
 
 from flask import Flask, request #import main Flask class and request object
+import re
 
 app = Flask(__name__) #create the Flask app
 
 @app.route('/query-example')
 def query_example():
-    return 'Todo...'
+
+	text = "I fixed T4523"
+
+	pattern = "[Tt][0-9]{4}"
+	matches = re.findall(pattern, text)
+
+	linkText =  "Phab tickets detected: "
+
+	for match in matches:
+    	phabLink = "phab.zenysis.com/" + match + " "
+    	linkText = linkText + phabLink
+
+    return linkText
 
 @app.route('/form-example')
 def form_example():
@@ -14,7 +27,17 @@ def form_example():
 
 @app.route('/json-example')
 def json_example():
-    return 'Todo...'
+    req_data = request.get_json()
+
+    text = req_data['text']
+    
+
+    return '''
+           The language value is: {}
+           The framework value is: {}
+           The Python version is: {}
+           The item at index 0 in the example list is: {}
+           The boolean value is: {}'''.format(language, framework, python_version, example, boolean_test)
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0') #run app in debug mode on port 5000
